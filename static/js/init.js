9e83e446a23cab4ca13689f115955c4392248660
lizhuo <lizhuogo@gmail.com>
2017-03-04 11:21:22 -0600

    ADD: new logo and signup modal

--- static/js/init.js
+++ static/js/init.js
@@ -1,8 +1,5 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function() {
+  $('.button-collapse').sideNav();
+  $('.parallax').parallax();
+  $('.modal').modal();
+});
