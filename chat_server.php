5fcef0a3e2b5ddf6cc3bfee947ea79edb5178833
lizhuo <lizhuogo@gmail.com>
2017-04-07 21:13:22 -0500

    chat done

---
+++ chat_server.php
@@ -0,0 +1,59 @@
+<?php
+include 'lib/websocket.class.php';
+
+class ChatServer extends WebSocket {
+  /**
+   * Dictionary for in-connection clients. Key is the user_id, value is the socket
+   * @var array
+   */
+  var $sockets_dict = array();
+
+  function process($user, $msg) {
+    $this->say("< ".$msg);
+    $this->log("in process".$user->socket);
+
+    // Decode messages
+    $json = json_decode($msg);
+    if (is_null($json)) { return; }
+
+    $sender_id = $json->{'user_id'};
+    $receiver_id = $json->{'receiver_id'};
+    $message = $json->{'msg'};
+    // Initialization message
+    if (strpos($msg, '[INITIALIZATION_CONFIG]') !== false) {
+      // $this->say("configuration");
+      $this->sockets_dict[$sender_id] = $user->socket;
+    } else {
+      $this->write_to_database($sender_id, $receiver_id, $message);
+
+      foreach ($this->sockets_dict as $key => $value) {
+        $this->say("$key -> $value");
+      }
+      if (array_key_exists($receiver_id, $this->sockets_dict)) {
+        $receiver_socket = $this->sockets_dict[$receiver_id];
+        $this->send($receiver_socket, $message);
+      } else {
+        $this->send($user->socket, 'Not online, save in DB');
+      }
+    }
+  }
+
+  function write_to_database($sender_id, $receiver_id, $message) {
+    $server_name = '127.0.0.1';
+    $username = 'upintheair_admin';
+    $password = 'admin411';
+    $db_name = 'upintheair_default';
+
+    $conn = new mysqli($server_name, $username, $password, $db_name);
+    if ($conn->connect_error) {
+      die('Connection failed: ' . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO messages (sender_id, receiver_id, message) VALUES ('$sender_id', '$receiver_id', '$message')";
+
+    if (!$conn->query($sql)) {
+      die('Database write failed: ' . $conn->connect_error);
+    }
+  }
+}
+$master = new ChatServer("localhost", 12345);
--- static/js/chat.jq.js
+++ static/js/chat.jq.js
@@ -1,9 +1,71 @@
-var source = new EventSource("api/message_controller.php");
-
-if(typeof(EventSource) !== "undefined") {
-    source.onmessage = function(event) {
-        document.getElementById("result").innerHTML += JSON.parse(event.data).time + "<br>";
-    };
-} else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support server-sent events...";
-}
+$(document).ready(function() {
+  $('#input-area').emoji({
+    showTab: true,
+    animation: 'fade',
+    button: '.emoji-button',
+    icons: [{
+      name: 'Baidu',
+      path: "/static/images/emoji/tieba/",
+      maxNum: 50,
+      file: ".jpg",
+      placeholder: ":{alias}:",
+      alias: {
+        1: "hehe",
+        2: "haha",
+        3: "tushe",
+        4: "a",
+        5: "ku",
+        6: "lu",
+        7: "kaixin",
+        8: "han",
+        9: "lei",
+        10: "heixian",
+        11: "bishi",
+        12: "bugaoxing",
+        13: "zhenbang",
+        14: "qian",
+        15: "yiwen",
+        16: "yinxian",
+        17: "tu",
+        18: "yi",
+        19: "weiqu",
+        20: "huaxin",
+        21: "hu",
+        22: "xiaonian",
+        23: "neng",
+        24: "taikaixin",
+        25: "huaji",
+        26: "mianqiang",
+        27: "kuanghan",
+        28: "guai",
+        29: "shuijiao",
+        30: "jinku",
+        31: "shengqi",
+        32: "jinya",
+        33: "pen",
+        34: "aixin",
+        35: "xinsui",
+        36: "meigui",
+        37: "liwu",
+        38: "caihong",
+        39: "xxyl",
+        40: "taiyang",
+        41: "qianbi",
+        42: "dnegpao",
+        43: "chabei",
+        44: "dangao",
+        45: "yinyue",
+        46: "haha2",
+        47: "shenli",
+        48: "damuzhi",
+        49: "ruo",
+        50: "OK"
+      }
+    }, {
+        name: "QQ",
+        path: "/static/images/emoji/qq/",
+        maxNum: 91,
+        file: ".gif"
+    }]
+  });
+});
